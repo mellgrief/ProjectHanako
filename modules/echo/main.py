e87ca1d9940770c69e208865958d7798a0a911c4
MellGrief <dedhost@yandex.ru>
2019-03-10 01:25:09 +0300

    change

--- modules/echo/main.py
+++
@@ -1,9 +0,0 @@
-author = "kiriharu"
-name = "Echo bot"
-description = '''Simple echo bot. Returns the message that was sent by user'''
-version = '0.1'
-command = '/echo'
-
-
-def handle(message, bot):
-    bot.reply_to(message, message.text)
--- modules/hanakompd/config.py
+++
@@ -1,5 +0,0 @@
-host = 'localhost' # host to connect to
-port = 6600 # MPD control port
-password = None # if None, no auth used
-
-
